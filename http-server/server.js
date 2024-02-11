d9718204edabf33d4c8a635115eb5d0758650091
Beck <rebecca.pond@keyin.com>
2024-02-11 16:00:34 -0330

    created more pages filled with some information

--- http-server/server.js
+++ http-server/server.js
@@ -1,47 +1,48 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
     // Parsing the requested URL
     const url = req.url;
 
-    // Using a switch statement to determine the action based on the route
-    switch (url) {
-        case '/':
-            console.log('Requested route: /');
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write('<h1>Welcome to the homepage!</h1>');
-            res.end();
-            break;
-        case '/about':
-            console.log('Requested route: /about');
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write('<h1>About Us</h1>');
-            res.end();
-            break;
-        case '/contact':
-            console.log('Requested route: /contact');
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write('<h1>Contact Us</h1>');
-            res.end();
-            break;
-        case '/products':
-            console.log('Requested route: /products');
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write('<h1>Our Products</h1>');
-            res.end();
-            break;
-        case '/subscribe':
-            console.log('Requested route: /subscribe');
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.write('<h1>Subscribe</h1>');
-            res.end();
-            break;
-        default:
-            console.log('Requested route not found');
-            res.writeHead(404, { 'Content-Type': 'text/html' });
-            res.write('<h1>404 Not Found</h1>');
-            res.end();
-            break;
+    // If the root URL is requested, serve index.html
+    if (url === '/') {
+        // Set file path to index.html
+        let filePath = './views/index.html';
+
+        // Read the HTML file from disk
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                // If file not found, respond with 404 Not Found
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.write('<h1>404 Not Found</h1>');
+                res.end();
+            } else {
+                // If file found, respond with the content of index.html
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(data);
+                res.end();
+            }
+        });
+    } else {
+        // For other routes, construct file path and serve corresponding HTML file
+        let filePath = './views' + url + '.html';
+
+        // Read the HTML file from disk
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                // If file not found, respond with 404 Not Found
+                res.writeHead(404, { 'Content-Type': 'text/html' });
+                res.write('<h1>404 Not Found</h1>');
+                res.end();
+            } else {
+                // If file found, respond with the content of the HTML file
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.write(data);
+                res.end();
+            }
+        });
     }
 });
 
@@ -50,3 +51,4 @@ const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
