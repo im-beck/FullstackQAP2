eaa147da79352f57848fb0bbe57fc0e7a5d2cb0e
Beck <rebecca.pond@keyin.com>
2024-02-11 16:35:03 -0330

    did 2 or 3 events

--- http-server/server.js
+++ http-server/server.js
@@ -1,6 +1,11 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const EventEmitter = require('events');
+
+// Instantiate an Event Emitter
+class MyEmitter extends EventEmitter {}
+const myEmitter = new MyEmitter();
 
 const server = http.createServer((req, res) => {
     // Parsing the requested URL
@@ -8,6 +13,16 @@ const server = http.createServer((req, res) => {
 
     // Mapping requested URL to file path
     let filePath = './views' + (url === '/' ? '/index' : url) + '.html';
+
+    // Emit events when routes are accessed
+    if (url === '/') {
+        myEmitter.emit('routeAccess', '/');
+    } else {
+        myEmitter.emit('routeAccess', url);
+        if (url !== '/') {
+            myEmitter.emit('nonHomeRouteAccess', url);
+        }
+    }
 
     // Read the HTML file from disk
     fs.readFile(filePath, (err, data) => {
@@ -21,6 +36,9 @@ const server = http.createServer((req, res) => {
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.write(data);
             res.end();
+
+            // Emit event for successful file read
+            myEmitter.emit('fileRead', filePath);
         }
     });
 });
@@ -29,4 +47,19 @@ const PORT = 3000;
 
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+// Emitting events for route access
+myEmitter.on('routeAccess', (route) => {
+    console.log(`Route accessed: ${route}`);
+});
+
+// Emitting event for non-home routes
+myEmitter.on('nonHomeRouteAccess', (route) => {
+    console.log(`Non-home route accessed: ${route}`);
+});
+
+// Emitting event for successful file read
+myEmitter.on('fileRead', (filePath) => {
+    console.log(`File read successfully: ${filePath}`);
 });
