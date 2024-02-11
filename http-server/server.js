676ad307a5dc26248971c221963f3b110b40c592
Beck <rebecca.pond@keyin.com>
2024-02-11 16:15:44 -0330

    finished up the server.js

--- http-server/server.js
+++ http-server/server.js
@@ -6,44 +6,23 @@ const server = http.createServer((req, res) => {
     // Parsing the requested URL
     const url = req.url;
 
-    // If the root URL is requested, serve index.html
-    if (url === '/') {
-        // Set file path to index.html
-        let filePath = './views/index.html';
+    // Mapping requested URL to file path
+    let filePath = './views' + (url === '/' ? '/index' : url) + '.html';
 
-        // Read the HTML file from disk
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                // If file not found, respond with 404 Not Found
-                res.writeHead(404, { 'Content-Type': 'text/html' });
-                res.write('<h1>404 Not Found</h1>');
-                res.end();
-            } else {
-                // If file found, respond with the content of index.html
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write(data);
-                res.end();
-            }
-        });
-    } else {
-        // For other routes, construct file path and serve corresponding HTML file
-        let filePath = './views' + url + '.html';
-
-        // Read the HTML file from disk
-        fs.readFile(filePath, (err, data) => {
-            if (err) {
-                // If file not found, respond with 404 Not Found
-                res.writeHead(404, { 'Content-Type': 'text/html' });
-                res.write('<h1>404 Not Found</h1>');
-                res.end();
-            } else {
-                // If file found, respond with the content of the HTML file
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.write(data);
-                res.end();
-            }
-        });
-    }
+    // Read the HTML file from disk
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            // If file not found, respond with 404 Not Found
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.write('<h1>404 Not Found</h1>');
+            res.end();
+        } else {
+            // If file found, respond with the content of the HTML file
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.write(data);
+            res.end();
+        }
+    });
 });
 
 const PORT = 3000;
@@ -51,4 +30,3 @@ const PORT = 3000;
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
-
